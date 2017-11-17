fff6df6cae23acb80934529fd95de94460a7c1b3
xiao <569922571@qq.com>
2017-11-17 17:57:03 +0800

    vuex 配合 axios 开发中

--- src/main.js
+++ src/main.js
@@ -3,22 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
-import {Row,Col,Collapse,CollapseItem } from 'element-ui'
+import MeHeader from '@/components/common/header'
+import {Row,Col,Collapse,CollapseItem,Form,FormItem,Input,Button } from 'element-ui'
+
 Vue.config.productionTip = false;
 Vue.use(MintUI);
 //let toPage = "errorPage";
 let toPage = "user";
+Vue.component('me-header',MeHeader);
 Vue.use(Row);
 Vue.use(Col);
 Vue.use(Collapse);
 Vue.use(CollapseItem);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Button);
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App },
   beforeCreate:function () {
@@ -27,6 +37,7 @@ new Vue({
 
   },
   created:function () {
+    //this.$store.dispatch('getMineBaseApi');//获取app基础信息
     //跳转到 toPage 页面
     this.$router.push(toPage);
     //结束载入loading..
